e5351f89797fde6afbf9c8f99be9bf92d25b40bb
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-03 13:41:58 +0700

    Error handling with custom errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,16 +5,22 @@ function EventsPage() {
   //get data from params routing loader
   const data = useLoaderData();
   const events = data.events;
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsasdfasdf");
 
   if (!response.ok) {
-    //...
+    // return { isError: true, message: "Could not fetch events." };
+    throw { message: "Could not fetch events." };
   } else {
     return response;
   }
