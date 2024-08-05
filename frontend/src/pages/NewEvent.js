62f22380da50c0cad435b72d42891a81bb7c9949
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-05 23:22:54 +0700

    reusing actions via request methods

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,37 +1,36 @@
-import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
-  return <EventForm />;
+  return <EventForm method="post" />;
 }
 
 export default NewEventPage;
 
-export async function action({ request, params }) {
-  const data = await request.formData();
+// export async function action({ request, params }) {
+//   const data = await request.formData();
 
-  const eventData = {
-    title: data.get("title"),
-    image: data.get("image"),
-    date: data.get("date"),
-    description: data.get("description"),
-  };
+//   const eventData = {
+//     title: data.get("title"),
+//     image: data.get("image"),
+//     date: data.get("date"),
+//     description: data.get("description"),
+//   };
 
-  const response = await fetch("http://localhost:8080/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
+//   const response = await fetch("http://localhost:8080/events", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(eventData),
+//   });
 
-  if (response.status === 422) {
-    return response;
-  }
+//   if (response.status === 422) {
+//     return response;
+//   }
 
-  if (!response.ok) {
-    throw json({ message: "Could not save event." }, { status: 500 });
-  }
+//   if (!response.ok) {
+//     throw json({ message: "Could not save event." }, { status: 500 });
+//   }
 
-  return redirect("/events");
-}
+//   return redirect("/events");
+// }
